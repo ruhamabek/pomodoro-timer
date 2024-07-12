86044078e8bb5e6f8a2d15a88852aaef9239fe1d
ruhamabek <ruhamauzubo@gmail.com>
2024-07-12 18:54:10 +0300

    generating the HTML of pomodoro timer and making start interactive

---
+++ scripts/pomodoro.js
@@ -0,0 +1,39 @@
+function renderPomodoro() {
+  let pomodoroHTML = '';
+  pomodoroHTML += `
+  <h1>Pomodoro Timer</h1>
+    <div class="timer-display">
+      <span id="minutes" class = "js-minutes">25</span>
+      <span>:</span>
+      <span id="seconds" class = "js-seconds">00</span>
+    </div>
+    <div class="controls">
+      <button id="start-button" class = "js-start-button">Start</button>
+      <button id="stop-button" disabled class = "js-stop-button">Stop</button>
+    </div>
+  `;
+  return pomodoroHTML;
+}
+document.querySelector('.js-container')
+    .innerHTML = renderPomodoro();
+
+document.querySelector('.js-start-button')
+    .addEventListener('click',()=>{
+     document.querySelector('.js-minutes').innerHTML = countDownMinutes();
+    });
+
+    function countDownMinutes() {
+      let minutes = parseInt(document.querySelector('.js-minutes').innerHTML, 10); 
+    
+      if (minutes === 25) {
+        const intervalId = setInterval(() => {
+          minutes--;
+          document.querySelector('.js-minutes').textContent = minutes; 
+    
+          if (minutes === 0) {
+            clearInterval(intervalId);
+           
+          }
+        }, 1000); 
+      }
+    }
