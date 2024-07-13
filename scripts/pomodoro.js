abfae9f83e74b63b9df983bb21f05b2a25df0a8b
ruhamabek <ruhamauzubo@gmail.com>
2024-07-13 12:08:49 +0300

    Make the seconds count

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -9,7 +9,7 @@ function renderPomodoro() {
     </div>
     <div class="controls">
       <button id="start-button" class = "js-start-button">Start</button>
-      <button id="stop-button" disabled class = "js-stop-button">Stop</button>
+      <button id="stop-button" class = "js-stop-button">Stop</button>
     </div>
   `;
   return pomodoroHTML;
@@ -20,6 +20,7 @@ document.querySelector('.js-container')
 document.querySelector('.js-start-button')
     .addEventListener('click',()=>{
      document.querySelector('.js-minutes').innerHTML = countDownMinutes();
+     document.querySelector('.js-seconds').innerHTML = countDownSeconds();
     });
 
     function countDownMinutes() {
@@ -38,3 +39,41 @@ document.querySelector('.js-start-button')
         return minutes;
       }
     }
+
+    
+     function countDownSeconds() {
+      let minutes = parseInt(document.querySelector('.js-minutes').innerHTML, 10);
+      let seconds = parseInt(document.querySelector('.js-seconds').innerHTML, 10);
+      while(minutes !== 0) {
+      seconds = 59;
+      minutes--;
+      countDownMinutes();
+      document.querySelector('.js-seconds').textContent = seconds; 
+      const intervalId = setInterval(() => { 
+           seconds--;
+           document.querySelector('.js-seconds').textContent =seconds; 
+     
+           if (seconds === 0) {
+             clearInterval(intervalId);
+             seconds = 59;
+             document.querySelector('.js-seconds').textContent = seconds;
+             if(seconds === 59) {
+        const intervalId2 = setInterval(()=>{
+                  seconds--;
+                  document.querySelector('.js-seconds').textContent = seconds;
+          if (seconds === 0) {
+            clearInterval(intervalId2);
+            countDownSeconds();
+          }
+              } 
+              , 1000); 
+             }
+            return seconds;
+             }
+            }, 1000);
+            return seconds;
+           } 
+         } 
+
+
+      
